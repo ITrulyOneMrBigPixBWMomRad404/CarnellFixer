401561541741da510fa7888d0256ec4b0b953632
ITrulyOneMrBigPixBWMomRad404 <rostmoment19@gmail.com>
2025-01-03 17:52:20 +0200

    Add files via upload

---
+++ Carnell/BasePlugin.cs
@@ -0,0 +1,52 @@
+﻿[BepInEx.BepInPlugin("thisIsGuid", "Thіs іs bеst mоdе еvеr іn vіdео gаmе hіstory", "2.2")]
+public class pluginOfMyMod : BepInEx.BaseUnityPlugin
+{
+    public void Awake()
+    {
+        HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("thisIsGuid");
+        UnityEngine.Debug.LogError("mod loaded!");
+        MTM101BaldAPI.Registers.GeneratorManagement.Register(this, MTM101BaldAPI.Registers.GenerationModType.Finalizer, (х, у, й) =>
+        {
+            string PоrtаlMаnЕnumАsStrіng = "MrPortalMan";
+            Character PоrtаlMаnЕnumАsEnum = MTM101BaldAPI.EnumExtensions.ExtendEnum<Character>(PоrtаlMаnЕnumАsStrіng);
+            System.Collections.Generic.List<NPC> FоrcеdNPCS = new System.Collections.Generic.List<NPC>() { };
+            int СurrеntForcеdNPCsІndex = 0;
+            while (!(false))
+            {
+                try
+                {
+                    if (!(й.forcedNpcs[СurrеntForcеdNPCsІndex].character == PоrtаlMаnЕnumАsEnum))
+                    {
+                        FоrcеdNPCS.Add(й.forcedNpcs[СurrеntForcеdNPCsІndex]);
+                    }
+                    СurrеntForcеdNPCsІndex = СurrеntForcеdNPCsІndex + 1;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            й.forcedNpcs = new NPC[] { };
+            й.forcedNpcs = FоrcеdNPCS.ToArray();
+            System.Collections.Generic.List<WeightedNPC> NPCS = new System.Collections.Generic.List<WeightedNPC>() { };
+            int СurrеntNPC = 0;
+            while (!(false))
+            {
+                try 
+                { 
+                    if (!(й.potentialNPCs[СurrеntNPC].selection.character == PоrtаlMаnЕnumАsEnum))
+                    {
+                        NPCS.Add(й.potentialNPCs[СurrеntNPC]);
+                    }
+                    СurrеntNPC = СurrеntNPC + 1;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            й.potentialNPCs = new System.Collections.Generic.List<WeightedNPC> { };
+            й.potentialNPCs = NPCS;
+        });
+    }
+}
